0d76e27943d811398f73c6d56153fd390eb2e772
FGtatsuro <fujiistorage@gmail.com>
2016-04-21 02:10:50 +0900

    Check whether extra components are installed on TravisCI.

--- spec/android_spec.rb
+++ spec/android_spec.rb
@@ -13,6 +13,11 @@ describe command('adb version'), :if => os[:family] == 'darwin' do
   its(:exit_status) { should eq 0 }
 end
 
+describe command('which aapt'), :if => os[:family] == 'darwin' do
+  its(:exit_status) { should eq 0 }
+end
+
+# On Linux, we also check extra components.
 describe command("#{ENV['ANDROID_HOME']}/tools/android list target"), :if => ['debian', 'alpine'].include?(os[:family]) do
   its(:exit_status) { should eq 0 }
 end
@@ -23,4 +28,20 @@ end
 
 describe command("#{ENV['ANDROID_HOME']}/platform-tools/adb version"), :if => ['debian', 'alpine'].include?(os[:family]) do
   its(:exit_status) { should eq 0 }
+end
+
+describe file("#{ENV['ANDROID_HOME']}/build-tools/23.0.1/aapt"), :if => ['debian', 'alpine'].include?(os[:family]) do
+  it { should exist }
+end
+
+describe file("#{ENV['ANDROID_HOME']}/extras/android/m2repository/source.properties"), :if => ['debian', 'alpine'].include?(os[:family]) do
+  it { should exist }
+end
+
+describe file("#{ENV['ANDROID_HOME']}/platforms/android-22/android.jar"), :if => ['debian', 'alpine'].include?(os[:family]) do
+  it { should exist }
+end
+
+describe file("#{ENV['ANDROID_HOME']}/system-images/android-17/default/x86/system.img"), :if => ['debian', 'alpine'].include?(os[:family]) do
+  it { should exist }
 end
